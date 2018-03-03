94f961ee0b350bea6711657b33fe0ecbcaa37bca
Bianca Besciu <bianca.besciu@gmail.com>
2018-03-03 14:50:36 +0100

    implementing reblur function

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 window.onload = init;
 function init() {
-    let img = document.getElementById('zero');
-    img.onclick = showAnswer;
+    let img = document.getElementsByTagName('img');
+    for(let i = 0; i < img.length; i++) {
+        img[i].onclick = showAnswer;
+    }
 }
 
-function showAnswer() {
-    let img = document.getElementById('zero');
-    img.src = "img/zero.jpg";
+function showAnswer(eventObj) {
+    let img = eventObj.target;
+    let name = img.id;
+    name = 'img/' + name + '.jpg';
+    img.src = name;
+
+    setTimeout(reblur, 2000, img);
+}
+
+function reblur(img) {
+    let name = img.id;
+    name = 'img/' + name + 'blur.jpg';
+    img.src = name;
 }
